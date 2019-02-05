9ad684d287354201f9e70139cc7270e22d5cc297
Kiara Contreras <kcontreras01@gmail.com>
2019-02-05 16:29:18 -0500

    multiple books to one author

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,15 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
 var books = [
 	{name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
 	{name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
 	{name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
+	{name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
+	{name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
+	{name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'},
 ];
 
 var authors = [
@@ -35,7 +38,12 @@ const AuthorType = new GraphQLObjectType({
 	fields: () => ({
 		id: {type: GraphQLID},
 		name: {type: GraphQLString},
-		age: {type: GraphQLInt}
+		age: {type: GraphQLInt},
+		books: {type: GraphQLList(BookType),
+			resolve(parent, args) {
+				return _.filter(books, {authorId: parent.id})
+			}
+		}
 	})
 });
 
